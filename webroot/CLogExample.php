9eda8cb5ca8dad2fe6f5def12dd5a3a4ff3b01d9
Sinon Eddeland <eddeland@gmail.com>
2016-09-03 22:09:08 +0200

    Added example file in webroot

---
+++ webroot/CLogExample.php
@@ -0,0 +1,36 @@
+<?php
+
+//
+// Set the error reporting.
+//
+error_reporting(-1);              // Report all type of errors
+ini_set('output_buffering', 0);   // Do not buffer outputs, write directly
+
+//
+// Get required files
+//
+require "../src/Logger/CLog.php";
+
+// Create new CLog object, set precision to 2 decimals
+$log = new \ultimadark\Logger\CLog(2);
+
+$log->timestamp("ExampleClass", "Beginning of file");
+
+// Calculate some meaningless stuff
+$log->timestamp("ExampleClass", "calculateBigSum", "Before summing");
+calculateBigSum();
+$log->timestamp("ExampleClass", "calculateBigSum", "After summing");
+
+$log->timestamp("ExampleClass", "End of file");
+
+// Print log
+echo $log->timestampAsTable();
+
+
+function calculateBigSum()
+{    
+    $sum = 0;
+    for ($i = 0; $i < 500000; $i++) {
+        $sum += $i;
+    }    
+}
