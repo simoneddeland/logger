bc35aa080785c5f8ee198b56f359cdec3f1ee490
Sinon Eddeland <eddeland@gmail.com>
2016-09-04 23:35:29 +0200

    Added more tests for CLog

--- test/Logger/CLogTest.php
+++ test/Logger/CLogTest.php
@@ -26,4 +26,45 @@ class CLogTest extends \PHPUnit_Framework_TestCase
         $exp = 2;
         $this->assertEquals($res, $exp, "The number of timestamps does not match");
     }
+
+    /**
+     * Test
+     *
+     * @return void
+     *
+     */
+    public function testTableHTML()
+    {
+        $log = new \ultimadark\Logger\CLog();
+        
+        $log->timestamp("test", "test");
+        $log->timestamp("test2", "test2");
+
+        $res = $log->timestampAsTable();
+
+        $exp = 'logtable';
+        $this->assertContains($res, $exp, "The table is not using the correct css class");
+
+    }    
+
+    /**
+     * Test
+     *
+     * @return void
+     *
+     */
+    public function testMostTimeConsumingDomain()
+    {
+        $log = new \ultimadark\Logger\CLog();
+        
+        $log->timestamp("testdomain", "test");
+        $log->timestamp("testdomain", "test");
+
+        $res = $log->timestampAsTable();
+
+        $exp = 'testdomain';
+        $this->assertEquals($res, $exp, "The most time consuming domain is not correct.");
+
+    }   
+
 }
